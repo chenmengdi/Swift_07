2d4946076a7dcfbce8b529b2dc6453fce1fd0445
chenmengdi <1599126082@qq.com>
2017-11-28 15:37:39 +0800

    Initial Commit

---
+++ Swift_07Tests/Swift_07Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_07Tests.swift
+//  Swift_07Tests
+//
+//  Created by 陈孟迪 on 2017/11/28.
+//  Copyright © 2017年 陈孟迪. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_07
+
+class Swift_07Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
